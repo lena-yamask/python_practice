06ca7dd46f178ee3a5a4fe32e5c297f57f9d18c0
lena-yamask <saki2lena018@gmail.com>
2024-09-30 18:27:31 +0900

    リストの使い方

--- helloworld.py
+++ helloworld.py
@@ -78,3 +78,41 @@ print(str(time) + "時")  # 数字->文字列
 strtoint = "3"
 print(int(strtoint) * 2)  # 文字列ー＞数字（整数）
 print(float(strtoint) * 2.0)  # 文字列ー＞数字（浮動小数点数）
+
+li = [5, "a", ["b", "c"]]  # リストの作り方
+# 型はバラバラでも良いが推奨されず、型は統一した方がいい
+# 空リストー＞s[]など要素を何も指定しないと作れる
+print(li[1])  # インデックスを指定する
+print(li[-1])
+# li[i] iの値が負の時、リスト li の要素数 + i(=末尾からi番目)の要素を取得
+# li[i:j] でiからj番目の要素を取得
+# 文字列と同じように、インデックス指定省略可能
+print(li[:])  # 先頭から末尾まで取得
+print(len(li))  # リストの長さを取得
+# a + b のようにリスト a とリスト b を結合すると、新しいリストが作成される
+# li * n のようにリスト li を n 回反復すると、新しいリストが作成される
+
+# li.append(e) のようにリストの append メソッドを使うと、
+# リスト li の末尾に要素 e を追加することができる
+print(li.append(4))
+# li.pop() のようにリストの pop メソッドを使うと、
+# リスト li の末尾の要素を削除して、その値を取得することができる
+print(li.pop())
+
+# li.remove(e) でリスト li から要素 e を削除することができる
+# リスト li の要素として e が複数含まれる場合、最初に現われるものを削除する
+li.remove("a")
+print(li, len(li))
+# del li[i] でリスト li のインデックス i の要素を削除することができる
+del li[1]
+print(li, len(li))
+# del li[i:j] で i から j-1 までの要素を削除
+
+li2 = [1, 3, 2]
+print(sorted(li2))  # 昇順でソート（上書きしない）
+li2.sort()  # 昇順（小さい順）に破壊的ソート（上書き）
+print(li2)
+li3 = ["apple", "cat", "banana"]
+print(sorted(li3, reverse=True))  # 降順でソート（上書きしない）
+li3.sort(reverse=True)  # 降順で破壊的ソート（上書き）
+# 異なる型が混在するリストは、ソート/破壊的ソートはできない
